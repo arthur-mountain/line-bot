8cf96c685ee826f34a6755a5140e3c201ad38afe
arthur <arthur.oceanus@gmail.com>
2022-05-05 23:07:20 +0800

    remove notion service

--- notion/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const getHasMoreParams = (id, response, page_size) => {
-  if (!response.has_more) return null;
-
-  return {
-    block_id: id,
-    start_cursor: response.next_cursor,
-    page_size
-  }
-};
